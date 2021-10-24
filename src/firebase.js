d557661c8638217cccbdb6a78514cdc5dbf5f513
snorkel875 <mianqiangyong71@gmail.com>
2021-10-24 21:33:53 +0900

    create app

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzRKclxxo_J4bw59NrK6PThK-4nNV6pF0",
+  authDomain: "idobatakaigi-with-hamu-a4f00.firebaseapp.com",
+  projectId: "idobatakaigi-with-hamu-a4f00",
+  storageBucket: "idobatakaigi-with-hamu-a4f00.appspot.com",
+  messagingSenderId: "147481387889",
+  appId: "1:147481387889:web:7e4614d0fa03bab8298cd0"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const messageRef = database.ref('messages');
+
+export const pushMessage = ({ name, text }) => {
+  messageRef.push({ name, text });
+};
