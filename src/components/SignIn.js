6cdfb6845175a2c94b1337420c81cfe308b1c6fd
snorkel875 <mianqiangyong71@gmail.com>
2021-10-24 15:58:32 +0900

    save name in japanese

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -48,6 +48,8 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(true);
   const [string, setString] = useState('');
+  const [isComposed, setIsComposed] = useState(false);
+  console.log({ disabled, string, isComposed });
 
   useEffect(() => {
     const disabled = string === '';
@@ -73,11 +75,15 @@ export default function SignIn({ setName }) {
             autoFocus
             onChange={(e) => setString(e.target.value)}
             onKeyDown={(e) => {
+              if (isComposed) return;
+
               if (e.key === 'Enter') {
                 setName(e.target.value);
                 e.preventDefault();
               }
             }}
+            onCompositionStart={()=> setIsComposed(true)}
+            onCompositionEnd={()=> setIsComposed(false)}
           />
           <Button
             type="button"
