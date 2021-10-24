411430ad576752824c71aee35f897619f61e0182
snorkel875 <mianqiangyong71@gmail.com>
2021-10-24 09:37:01 +0900

    fixed setName by click button

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import SignIn from './SignIn';
 
 export default () => {
-  const { name, setName } = useState('');
+  const [name, setName] = useState('');
 
   return <SignIn setName={setName} />;
 };
